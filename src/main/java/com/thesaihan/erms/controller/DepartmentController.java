2ebd91c4c64cfd3107ed47cae4be2c91eaa48800
Sai Han <45680003+thesaihan@users.noreply.github.com>
2020-03-19 20:51:43 +0630

    Update DepartmentController.java

--- src/main/java/com/thesaihan/erms/controller/DepartmentController.java
+++ src/main/java/com/thesaihan/erms/controller/DepartmentController.java
@@ -1,6 +1,12 @@
 package com.thesaihan.erms.controller;
 
-@Controller("department")
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("department")
 public class DepartmentController {
 
 }
